a946ef2c542fcdc0207d4d1557d02ba8b3523a60
Joe DiFebo <jdifebo@nexient.com>
2016-09-29 18:19:47 -0400

    connected frontend to backend!!!!!

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+"use strict";
+
 $("#fact-list").empty();
 
-$.getJSON("test_resources/facts.json", null, function callback(facts){
+$.getJSON("http://ec2-52-90-84-9.compute-1.amazonaws.com:8080/backend/facts", null, function callback(facts){
     for (var i = 0; i < facts.length; i++){
         $("#fact-list").append(buildCard(facts[i].title, facts[i].date, facts[i].body, facts[i].sources));
     }
@@ -12,23 +14,31 @@ $.getJSON("test_resources/facts.json", null, function callback(facts){
  */
 function buildCard(title, date, body, sources){
     return '<div class="card">' + 
-            '<div class="card-header">' + 
-            '<h4 class="card-title">' + 
-            title + 
-            '</h4>' + 
-            '<h6 class="card-subtitle text-muted">' + 
-            date + 
-            '</h6>' + 
-            '</div>' + 
-            '<div class="card-block">' + 
-            '<p class="card-text">' + 
-            body + 
-            '</p>' + 
-            '<a href="' + 
-            sources[0].url + 
-            '" class="card-link">' + 
-            sources[0].name + 
-            '</a>' + 
-            '</div>' + 
+                '<div class="card-header">' + 
+                    '<h4 class="card-title">' + 
+                        title + 
+                    '</h4>' + 
+                    '<h6 class="card-subtitle text-muted">' + 
+                        date + 
+                    '</h6>' + 
+                '</div>' + 
+                '<div class="card-block">' + 
+                    '<p class="card-text">' + 
+                        body + 
+                    '</p>' + 
+                    buildSourceLinks(sources) + 
+                '</div>' + 
             '</div>';
+}
+
+function buildSourceLinks(sources){
+    var html = "";
+    for (var i = 0; i < sources.length; i++){
+        html += '<a href="' + 
+                sources[i].url + 
+                '" class="card-link">' + 
+                sources[i].name + 
+                '</a>';
+    }
+    return html;
 }
