8513b700e861571aa63eab2a83b9847e00729f7d
Joe DiFebo <jdifebo@nexient.com>
2016-10-02 12:52:57 -0400

    archives only displays valid months

--- frontend/script.js
+++ frontend/script.js
@@ -1,55 +1,76 @@
 "use strict";
 
 $("#fact-list").empty();
+$("#archives").empty();
 
-$.getJSON("http://ec2-52-55-8-54.compute-1.amazonaws.com:8080/backend/facts", null, function callback(facts){
-    console.log(typeof facts)
-    facts.map(function setDate(fact) {
+$.getJSON("http://ec2-52-55-8-54.compute-1.amazonaws.com:8080/backend/facts", null, function callback(facts) {
+    // Convert date field to actual date objects and sort
+    facts = facts.map(function setDate(fact) {
         fact.date = new Date(fact.date);
         return fact;
-    }).sort(function compare(fact1, fact2){
+    }).sort(function compare(fact1, fact2) {
         return fact1.date - fact2.date;
-    }).forEach(function modifyHtml(fact){
+    })
+
+    // Populate facts
+    facts.forEach(function modifyHtml(fact) {
         $("#fact-list").prepend(buildCard(fact.title, fact.date, fact.body, fact.sources));
     });
-    // for (var i = 0; i < facts.length; i++){
-    //     facts[i].date = new Date(facts[i].date);
-    //     console.log(facts[i].date.toDateString());
-    // }
-    console.log(JSON.stringify(facts));
+
+    // TODO: this is getting too messy, I should bring in a date library
+    var monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ];
+
+    var monthAndYearSet = {};
+    facts.map(function mapToMonthOnly(fact) {
+        return monthNames[fact.date.getMonth()] + " " + fact.date.getFullYear();
+    }).forEach(function addToSet(date) {
+        monthAndYearSet[date] = date;
+    });
+    for (var month in monthAndYearSet) {
+        $("#archives").prepend(buildArchiveLink(month));
+    }
 });
 
+function buildArchiveLink(month){
+    return '<li><a href="#">' + 
+    month + '</a></li>';
+}
 
 /**
  * lol quick and dirty, but oh so dirty
  */
-function buildCard(title, date, body, sources){
-    return '<div class="card">' + 
-                '<div class="card-header"  data-toggle="collapse" data-target="#'+date.getTime()+'">' + 
-                    '<h4 class="card-title">' + 
-                        title + 
-                    '</h4>' + 
-                    '<h6 class="card-subtitle text-muted">' + 
-                        date.toDateString() + 
-                    '</h6>' + 
-                '</div>' + 
-                '<div class="card-block collapse in" id="'+date.getTime()+'">' + 
-                    '<p class="card-text">' + 
-                        body + 
-                    '</p>' + 
-                    buildSourceLinks(sources) + 
-                '</div>' + 
-            '</div>';
-} 
+function buildCard(title, date, body, sources) {
+    return '<div class="card">' +
+        '<div class="card-header"  data-toggle="collapse" data-target="#' + date.getTime() + '">' +
+        '<h4 class="card-title">' +
+        title +
+        '</h4>' +
+        '<h6 class="card-subtitle text-muted">' +
+        date.toDateString() +
+        '</h6>' +
+        '</div>' +
+        '<div class="card-block collapse in" id="' + date.getTime() + '">' +
+        '<p class="card-text">' +
+        body +
+        '</p>' +
+        buildSourceLinks(sources) +
+        '</div>' +
+        '</div>';
+}
 
-function buildSourceLinks(sources){
+function buildSourceLinks(sources) {
     var html = "";
-    for (var i = 0; i < sources.length; i++){
-        html += '<a href="' + 
-                sources[i].url + 
-                '" class="card-link">' + 
-                sources[i].name + 
-                '</a>';
+    for (var i = 0; i < sources.length; i++) {
+        html += '<a href="' +
+            sources[i].url +
+            '" class="card-link">' +
+            sources[i].name +
+            '</a>';
     }
     return html;
 }
